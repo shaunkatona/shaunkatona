46b74f9a7b20f533fb032a3210d8e87d6f1c5b4a
shaunkatona <shaunkatona@Shauns-MacBook-Pro.local>
2014-07-22 21:57:00 -0600

    buncha style changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,6 @@
 /**
  * Created by shaunkatona on 7/21/14.
  */
-$(".disappearAfterAnimation").on("animationend webkitAnimationEnd", function () {
-    $(this).addClass("none");
-});
-
 $("li", $("#icons")).on("animationstart webkitAnimationStart", function () {
     $(this).removeClass("invisible");
 });
